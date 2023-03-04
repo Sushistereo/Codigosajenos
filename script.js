be182e9320d249a5db635d91609db8afe129b62e
Belen Martin <bemartin@hotmail.com.ar>
2023-03-04 12:21:00 -0300

    js api get people

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-console.log("codigos ajenos")
-//esto es un comentario
+async function getPeople(){
+    //ir a buscar los datos
+    const response = fetch("https://randomuser.me/api/")
+    //const results = Object.fromEntries(response.results)
+    console.log(response)
+    //const people = response.results.map(()=>{
+    //    return {
+    //        name:`${name.first} ${name.last}`,
+    //        age: dob.age
+    //         }
+    //}) 
+    //return people.json()
+    //mapear los campos
 
-const boton = document.querySelector(.boton")
+}
 
-console.log(boton)
+window.onload = async()=>{
+    console.log("el texto que quieras")
+    const people = await getPeople()
+    console.log(people)
+}
+
